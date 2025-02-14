d86aa6e43e91d06d6831b3d43747e62b2845457a
jackrichter <jackrichter1@gmail.com>
2025-02-14 01:05:02 +0100

    Third microservice, loans as a maven module.

---
+++ loans/src/main/java/com/eazybytes/loans/service/impl/LoansServiceImpl.java
@@ -0,0 +1,92 @@
+package com.eazybytes.loans.service.impl;
+
+import com.eazybytes.loans.constants.LoansConstants;
+import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.entity.Loans;
+import com.eazybytes.loans.exception.LoanAlreadyExistsException;
+import com.eazybytes.loans.exception.ResourceNotFoundException;
+import com.eazybytes.loans.mapper.LoansMapper;
+import com.eazybytes.loans.repository.LoansRepository;
+import com.eazybytes.loans.service.ILoansService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class LoansServiceImpl implements ILoansService {
+
+    private LoansRepository loansRepository;
+
+    /**
+     * Only one loan with the same mobile number, meaning one loan per customer, is allowed.
+     *
+     * @param mobileNumber the mobile number of the customer
+     */
+    @Override
+    public void createLoan(String mobileNumber) {
+        Optional<Loans> existingLoan = loansRepository.findByMobileNumber(mobileNumber);
+        if (existingLoan.isPresent()) {
+            throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber: " + mobileNumber);
+        }
+        loansRepository.save(createNewLoan(mobileNumber));
+    }
+
+    /**
+     * Helper method to create a new loan
+     *
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new loan details
+     */
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+
+        Long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setMobileNumber(mobileNumber);
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+
+        return newLoan;
+    }
+
+    /**
+     * @param mobileNumber Input mobile number
+     * @return
+     */
+    @Override
+    public LoansDto fetchLoan(String mobileNumber) {
+        Loans loan = loansRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber));
+        return LoansMapper.mapToLoansDto(loan, new LoansDto());
+    }
+
+    /**
+     * @param loanDto - LoansDto Object
+     * @return
+     */
+    @Override
+    public boolean updateLoan(LoansDto loanDto) {
+        Loans existingLoan = loansRepository.findByLoanNumber(loanDto.getLoanNumber())
+                .orElseThrow(() -> new ResourceNotFoundException("Loan", "loanNumber", loanDto.getLoanNumber()));
+        Loans updatedLoan = LoansMapper.mapToLoans(loanDto, existingLoan);
+        loansRepository.save(updatedLoan);
+        return true;
+    }
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return
+     */
+    @Override
+    public boolean deleteLoan(String mobileNumber) {
+        Loans existingLoan = loansRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber));
+        loansRepository.deleteById(existingLoan.getLoanId());
+        return true;
+    }
+}
