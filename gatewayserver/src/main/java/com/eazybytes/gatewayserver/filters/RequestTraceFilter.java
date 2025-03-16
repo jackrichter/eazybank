7f061bf0e38e04ceb4d170117fe624ba800f130b
jackrichter <jackrichter1@gmail.com>
2025-03-16 19:25:05 +0100

    Another filter added (adds the correlationId in the response header).

---
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/RequestTraceFilter.java
@@ -0,0 +1,52 @@
+package com.eazybytes.gatewayserver.filters;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Order(1)
+@Component
+public class RequestTraceFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestTraceFilter.class);
+
+    @Autowired
+    private FilterUtility filterUtility;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        if (isCorrelationIdPresent(headers)) {
+            logger.debug("eazyBank-correlation-id found in RequestTraceFilter : {}", filterUtility.getCorrelationId(headers));
+        }
+        else {
+            String correlationId = generateCorrelationId();
+            exchange = filterUtility.setCorrelationId(exchange, correlationId);
+            logger.debug("eazyBank-correlation-id generated in RequestTraceFilter : {}", correlationId);
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders headers) {
+        if (filterUtility.getCorrelationId(headers) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String generateCorrelationId() {
+        return UUID.randomUUID().toString();
+    }
+}
